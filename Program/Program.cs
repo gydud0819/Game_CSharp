df353f4994595acfd6539e701d52d04e8ddd0d85
gydud0819 <pstar0819@naver.com>
2025-08-25 15:42:27 +0900

    (2025-08-25) C# 매개 변수 한정자

---
+++ Program/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Boxing
+            // 값 형식의 데이터를 참조 형식으로 변환하는 과정
+            // ex)
+            //int value = 100;
+            //
+            //object generic = value;
+            //
+            //Console.WriteLine("value : " + value);
+            //Console.WriteLine("generic : " + generic);
+            #endregion
+
+            #region Unboxing
+            // 참조 형식의 데이터를 값 형식으로 변화하는 과정
+            // ex)
+
+            //int box = (int)generic;     // 명시적 형변환을 해줘야함 (ex. (int))
+            //
+            //Console.WriteLine("box : " +  box);
+
+            // 요약: 박싱 언박싱이 안일어나게 하는 게 제일 조음 
+            #endregion
+
+            Utility utility = new Utility();
+
+            utility.Pause();
+
+            int x = 10;
+            int y = 20;
+            utility.Swap(ref x, ref y);
+
+            int a = -7;
+           
+            Console.WriteLine("절대값 : " + utility.Absolute(in a));
+        }
+    }
+}
---
+++ Program/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Utility
+    {
+        private int data;
+
+
+  
+        public void Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+          
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+
+        }
+
+        public int Absolute(in int value)
+        {
+            // 절대값 : 음수값도 양수로 출력되게 
+            return value < 0 ? -value : value;
+
+            
+        }
+    }
+}
